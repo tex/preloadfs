d3422d1cfea129303d7cd2f14810b80611ca0fd7
Milan Svoboda <milan.svoboda@centrum.cz>
2008-12-30 19:28:59 +0100

    Remove debug prints.

--- src/FBuffer.cpp
+++ src/FBuffer.cpp
@@ -24,15 +24,11 @@ FBuffer::~FBuffer()
 
 int FBuffer::write(int pos, char *buf, int len)
 {
-	std::cerr << __PRETTY_FUNCTION__ << ", pos: " << pos << ", len: " << len << std::endl;
-
 	return ::pwrite(m_fd, buf, len, pos);
 }
 
 int FBuffer::read(int pos, char *buf, int len)
 {
-	std::cerr << __PRETTY_FUNCTION__ << ", pos: " << pos << ", len: " << len << std::endl;
-
 	return ::pread(m_fd, buf, len, pos);
 }
 
